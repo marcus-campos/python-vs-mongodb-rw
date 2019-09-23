93e9b14b293c02cab0ced23688ff7bf18eb87729
Marcus Vinicius Campos <marcus_vinicius_campos@hotmail.com>
2019-09-23 16:49:34 -0300

    chore(startup.py) add configs

--- startup.py
+++ startup.py
@@ -4,6 +4,10 @@ from random import randint
 
 from pymongo import MongoClient
 
+#Step 0: Config
+MAX_REVIEWS=1000
+MAX_SEARCH=5000
+
 #Step 1: Connect to MongoDB - Note: Change connection string as needed
 client = MongoClient(port=27020)
 db=client.business
@@ -14,7 +18,7 @@ def insertReviews():
     company_type = ['LLC', 'Inc', 'Company', 'Corporation']
     company_cuisine = ['Pizza', 'Bar Food', 'Fast Food', 'Italian', 'Mexican', 'American', 'Sushi Bar', 'Vegetarian']
 
-    for x in range(1, 1001):
+    for x in range(1, MAX_REVIEWS):
         business = {
             'name' : names[randint(0, (len(names)-1))] + ' ' + names[randint(0, (len(names)-1))]  + ' ' + company_type[randint(0, (len(company_type)-1))],
             'rating' : randint(1, 5),
@@ -23,9 +27,9 @@ def insertReviews():
         #Step 3: Insert business object directly into MongoDB via isnert_one
         result=db.reviews.insert_one(business)
         #Step 4: Print to the console the ObjectID of the new document
-        print('Created {0} of 1000 as {1}'.format(x,result.inserted_id))
+        print('Created {0} of {1} as {2}'.format(x, MAX_REVIEWS, result.inserted_id))
     #Step 5: Tell us that you are done
-    print('Finished creating 1000 business reviews')
+    print('Finished creating {0} business reviews'.format(MAX_REVIEWS))
 
 #Step 6: Search data
 def searchReviews():
@@ -33,9 +37,9 @@ def searchReviews():
     company_type = ['LLC', 'Inc', 'Company', 'Corporation']
     company_cuisine = ['Pizza', 'Bar Food', 'Fast Food', 'Italian', 'Mexican', 'American', 'Sushi Bar', 'Vegetarian']
 
-    for x in range(1, 50001):
+    for x in range(1, MAX_SEARCH):
         result = db.reviews.find({"$text": {"$search": company_cuisine[randint(0, (len(company_cuisine)-1))]}}).limit(10)
-        print('Search {0} of 50000 - {1}'.format(x,result))
+        print('Search {0} of {1} - {2}'.format(x, MAX_SEARCH, result))
     #Step 7: Tell us that you are done
 
 if __name__ == "__main__":
@@ -44,6 +48,7 @@ if __name__ == "__main__":
     p1.start()
     p2 = Process(target=searchReviews)
     p2.start()
+
     p1.join()
     p2.join()
     elapsed = time.perf_counter() - s
